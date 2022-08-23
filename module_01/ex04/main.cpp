feedab8632c8a2aa1b1a10c623965908c7268041
Arthur <pigeon.arthur@hotmail.fr>
2022-08-23 14:10:53 +0000

    update error of la mierda

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -6,11 +6,12 @@
 /*   By: apigeon <apigeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 15:12:39 by apigeon           #+#    #+#             */
-/*   Updated: 2022/08/23 16:05:17 by apigeon          ###   ########.fr       */
+/*   Updated: 2022/08/23 14:09:42 by apigeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 
 static int	usage(std::string prog_name)
 {
@@ -35,15 +36,15 @@ int	main(int ac, char **av)
 	std::ifstream	in;
 
 	if (ac != 4)
-		exit(usage(av[0]));
+		return usage(av[0]);
 	to_replace = av[2];
 	len = to_replace.length();
 	in.open(av[1], std::ifstream::in);
 	if (!in) {
 		std::cout << "Unable to open the file\n";
-		exit(2);
+		return 2;
 	}
-	while (getline(in, line) {
+	while (getline(in, line)) {
 		while (true) {
 			size_t pos = line.find(av[2]);
 			if (pos != std::string::npos)
