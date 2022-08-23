244d09573ac8d9d8ea13700881e8457f3107be95
Arthur Pigeon <pigeon.arthur@hotmail.fr>
2022-08-23 17:53:54 +0200

    module 01 ex06 done

---
+++ module_01/ex06/Harl.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apigeon <apigeon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/23 16:52:37 by apigeon           #+#    #+#             */
+/*   Updated: 2022/08/23 17:50:48 by apigeon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.h"
+
+void	Harl::complain(std::string level)
+{
+	int	i;
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	i = 0;
+	while (i < 4) {
+		if (levels[i] == level)
+			break;
+		i++;
+	}
+	switch(i) {
+		case 0:
+			debug();
+		case 1:
+			info();
+		case 2:
+			warning();
+		case 3:
+			error();
+			break;
+		default:
+			std::cout << "[Probably complaining about insignicant problems]\n";
+	}
+}
+
+void	Harl::debug(void)
+{
+	std::cout << BLUE << "[DEBUG]" << RESET << ": message\n";
+}
+
+void	Harl::info(void)
+{
+	std::cout << GREEN << "[INFO]" << RESET << ": message\n";
+}
+
+void	Harl::warning(void)
+{
+	std::cout << YELLOW << "[WARNING]" << RESET << ": message\n";
+}
+
+void	Harl::error(void)
+{
+	std::cout << RED << "[ERROR]" << RESET << ": message\n";
+}
