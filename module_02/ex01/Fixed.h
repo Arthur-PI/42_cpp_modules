4ae3775693356a02536f6e2ffe58fb0e797fc299
Arthur Pigeon <pigeon.arthur@hotmail.fr>
2022-08-23 18:37:39 +0200

    init module 02 ex01

---
+++ module_02/ex01/Fixed.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apigeon <apigeon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/23 17:57:52 by apigeon           #+#    #+#             */
+/*   Updated: 2022/08/23 18:19:59 by apigeon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+# define FIXED_H
+
+#include <iostream>
+
+class Fixed
+{
+	public:
+		Fixed(void);
+		Fixed(const Fixed &fixed);
+		~Fixed(void);
+		Fixed & operator=(const Fixed &fixed);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+
+	private:
+		int	_value;
+		static const int	fractional_bits;
+};
+
+#endif
