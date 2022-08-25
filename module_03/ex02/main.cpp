694a9eaab8740bb955f8a0ed9794abef5aad34a3
Arthur Pigeon <pigeon.arthur@hotmail.fr>
2022-08-25 14:21:47 +0200

    module 03 ex02 main completed

--- module_03/ex02/main.cpp
+++ module_03/ex02/main.cpp
@@ -6,11 +6,12 @@
 /*   By: apigeon <apigeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 10:13:12 by apigeon           #+#    #+#             */
-/*   Updated: 2022/08/25 12:00:28 by apigeon          ###   ########.fr       */
+/*   Updated: 2022/08/25 14:21:05 by apigeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.h"
+#include "FragTrap.h"
 
 int	main(void)
 {
@@ -19,6 +20,7 @@ int	main(void)
 	ClapTrap	ct3("Thomas");
 	ScavTrap	st1("Téo");
 	ScavTrap	st2(st1);
+	FragTrap	ft1("Norman");
 
 	ct1.attack("Victor");
 	ct2.takeDamage(ct1.getAttack());
@@ -29,5 +31,7 @@ int	main(void)
 	ct2.beRepaired(10);
 	st1.guardGate();
 	st2.guardGate();
+	ft1.attack("Arthur");
+	ct1.takeDamage(ft1.getAttack());
 	return 0;
 }
