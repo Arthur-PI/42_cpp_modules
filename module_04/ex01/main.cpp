2457f79b8676880d60f433d6a3304024bfb7c7a5
Arthur Pigeon <pigeon.arthur@hotmail.fr>
2022-08-30 16:03:42 +0200

    module 04 ex01 update main

--- module_04/ex01/main.cpp
+++ module_04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: apigeon <apigeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 09:39:50 by apigeon           #+#    #+#             */
-/*   Updated: 2022/08/30 13:55:44 by apigeon          ###   ########.fr       */
+/*   Updated: 2022/08/30 16:02:55 by apigeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,17 @@ int	main(void)
 		cat2.getBrain()->setIdea(0, "Meow at sunshine");
 		std::cout << cat.getBrain()->getIdea(0) << std::endl;
 		std::cout << cat2.getBrain()->getIdea(0) << std::endl;
+	}
+	{
+		Animal	*animals[100];
+		for (int i=0; i < 100; i++) {
+			if (i % 2)
+				animals[i] = new Cat();
+			else
+				animals[i] = new Dog();
+		}
+		for (int i=0; i < 100; i++)
+			delete animals[i];
 	}
 
 	return (0);
