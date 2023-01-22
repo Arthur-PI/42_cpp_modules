0e488b07e750f8547b7995868022235be86541d4
Arthur PIGEON <apigeon@student.42.fr>
2023-01-22 12:35:54 +0100

    fix: module 02 exo03 bsp if on edge

--- module_02/ex03/bsp.cpp
+++ module_02/ex03/bsp.cpp
@@ -17,10 +17,19 @@ Fixed	sign(Point a, Point b, Point c)
 	return (a.getX() - c.getX()) * (b.getY() - c.getY()) - (b.getX() - c.getX()) * (a.getY() - c.getY());
 }
 
+bool	is_in_segment(Point a, Point b, Point c)
+{
+
+	Fixed	coef_a_b = ((b.getY() - a.getY()) / (b.getX() - a.getX()));
+	Fixed	coef_b_c = ((b.getY() - c.getY()) / (b.getX() - c.getX()));
+
+	return (coef_a_b == coef_b_c);
+}
+
 bool	bsp(Point const a, Point const b, Point const c, Point const point)
 {
 	Fixed	d1, d2, d3;
-	bool	has_neg, has_pos;
+	bool	has_neg, has_pos, is_in_triangle;
 
 	d1 = sign(point, a, b);
 	d2 = sign(point, b, c);
@@ -28,5 +37,12 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point)
 
 	has_neg = (d1 < Fixed(0)) || (d2 < Fixed(0)) || (d3 < Fixed(0));
 	has_pos = (d1 > Fixed(0)) || (d2 > Fixed(0)) || (d3 > Fixed(0));
-	return !(has_neg && has_pos);
+
+	is_in_triangle = !(has_neg && has_pos);
+	if (is_in_triangle) {
+		if (is_in_segment(a, b, point) || is_in_segment(a, c, point) || is_in_segment(b, c, point))
+			return false;
+		return true;
+	}
+	return false;
 }
--- module_02/ex03/main.cpp
+++ module_02/ex03/main.cpp
@@ -20,9 +20,11 @@ int	main(void)
 
 	Point	d(2, 1);
 	Point	e(2, 2.1);
+	Point	f(2, 2);
 	std::cout << "The Triangle is a(" << a.getX() << "," << a.getY() << "), b(" << b.getX() << "," << b.getY() << "), c(";
 	std::cout << c.getX() << "," << c.getY() << "):\n";
 	std::cout << "The point (" << d.getX() << "," << d.getY() << ") is in triangle ? " << bsp(a, b, c, d) << std::endl;
 	std::cout << "The point (" << e.getX() << "," << e.getY() << ") is in triangle ? " << bsp(a, b, c, e) << std::endl;
+	std::cout << "The point (" << f.getX() << "," << f.getY() << ") is in triangle ? " << bsp(a, b, c, f) << std::endl;
 	return 0;
 }
